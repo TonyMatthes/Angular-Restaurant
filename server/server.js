9fc619265961cb761adc6acc94210e04bb377bd0
Tony Matthes <tony@Tonys-MacBook-Pro.local>
2018-09-12 14:46:31 -0500

    stretch goal 1

---
+++ server/server.js
@@ -0,0 +1,37 @@
+// requires
+const express = require( 'express' );
+const bodyParser = require( 'body-parser' );
+const app = express();
+// uses
+app.use( express.static( 'server/public' ) );
+app.use( bodyParser.urlencoded( { extended: true } ) ); // <--- NEEDED FOR POST with JQ AJAX
+// globals
+const port = 5000;
+
+
+// spin up server
+
+app.listen( port, ()=>{
+    console.log( 'server is up on:', port );
+}) ;
+
+// end server up
+
+// get route
+
+// app.get( '/books', ( req, res )=>{
+//     console.log( 'in /books GET' );
+//     res.send( books );
+// }) 
+
+//end /books GET
+
+// post route
+
+// app.post( '/books', ( req, res )=>{
+//     console.log( 'in /books POST:', req.body );
+//     books.push( req.body );
+//     res.sendStatus( 200 );
+// })
+
+// end /books POST
